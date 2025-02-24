494c4f33263ff0a637eb7a470debade2bb0f90cc
Gedeon Ntigibeshya <g.ntigibesh@alustudent.com>
2025-02-24 12:52:28 +0200

    9-print_last_digit.py

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python3
 # This function prints the last digit of a number
-# @number: The number to be inputed by the user
-
+# @number: The number to be inputted by the user
 
 def print_last_digit(number):
     if number > 0:
         print(f"{number % 10}", end="")
         return (number % 10)
     elif number < 0:
-        print(f"{(-number % 10)}", end="")
-        return ((-number % 10))
+        print(f"{-number % 10}", end="")
+        return (-number % 10)
     else:
-        print(f"{number:}", end="")
-        return (number)i
-
+        print(f"{number}", end="")
+        return number  # Removed the 'i' that was causing the error
 
 '''
 This function can also be used
@@ -22,3 +20,4 @@ def print_last_digit(number):
     print(f"{abs(number) % 10}", end="")
     return (abs(number) % 10)
 '''
+
