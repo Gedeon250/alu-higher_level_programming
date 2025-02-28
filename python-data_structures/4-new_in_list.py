23e2eb2af6ee2ad018e16bbccebb8ffd0d43a415
Gedeon Ntigibeshya <g.ntigibesh@alustudent.com>
2025-02-28 12:28:23 +0200

    4-new_in_list.py

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,10 +1,25 @@
-#!/usr/bin/python3
-# Removes all cha c and C form a string
+#!/usr/bin/python3i
+# Replacees an element in a list at a specific position without modifying
 
 
-def no_c(my_string):
-    {ord('c'): None, ord('C'): None}
-    for i in my_string:
-        if ord('i') == 99 or 67:
-            my_string.translate({ord('c'): None, ord('C'): None})
-    return my_string.translate({ord('c'): None, ord('C'): None})
+def new_in_list(my_list, idx, element):
+    new_list = my_list.copy()
+    if idx < 0 or idx > (len(my_list) - 1):
+        return new_list
+    else:
+        new_list[idx] = element
+        return new_list
+
+# my_list = [1, 2, 3]
+# print(new_in_list(my_list, 1, 4))
+# print(my_list)
+'''
+def replace_in_list(my_list, idx, element):
+    new_list = []
+    if idx < 0 or idx > (len(my_list) - 1):
+        return my_list
+    else:
+        my_list[idx] = element
+        new_list = my_list
+        return new_list
+'''
