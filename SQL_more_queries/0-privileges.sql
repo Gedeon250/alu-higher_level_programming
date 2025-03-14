a075e270fda0d873dbbe8689f93dbe4a2a0d1104
Gedeon Ntigibeshya <g.ntigibesh@alustudent.com>
2025-03-14 16:11:28 +0200

    0-privileges.sql

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,18 @@
--- Lists all privileges of the users user_0d_1 and user_0d_2
+-- Adjust privileges for user_0d_1
+-- Revoke extra privileges that are not needed
+REVOKE AUDIT_ABORT_EXEMPT, AUTHENTICATION_POLICY_ADMIN, FIREWALL_EXEMPT, PASSWORDLESS_USER_ADMIN, SENSITIVE_VARIABLES_OBSERVER 
+FROM 'user_0d_1'@'localhost';
+
+-- Grant missing privileges if any
+GRANT SYSTEM_USER, SYSTEM_VARIABLES_ADMIN, BACKUP_ADMIN TO 'user_0d_1'@'localhost';
+
+-- Verify the changes
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- Adjust privileges for user_0d_2
+-- Grant only SELECT and INSERT privileges for user_0d_2
+REVOKE ALL PRIVILEGES, GRANT OPTION FROM 'user_0d_2'@'localhost';
+GRANT SELECT, INSERT ON user_2_db.* TO 'user_0d_2'@'localhost';
+
+-- Verify the changes
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
